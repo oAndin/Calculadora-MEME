b67662fad3fe598893db2db3f05e4fa0f5b261ad
oAndin <00andherson@gmail.com>
2023-05-23 19:09:47 -0300

    Refazendo

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function digaHW(){
-    console.log("Hello World!");
-};
-
-console.log(digaHW);
-
-setTimeout(function esperou() {
-    console.log("Ainda esperando?");
-}, 5000);
+console.log("Hello World!")
