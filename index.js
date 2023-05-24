ca02d8eb9242d9dbf6300915081aaa68f3f0a72a
oAndin <00andherson@gmail.com>
2023-05-24 16:40:53 -0300

    Preciso assistir os videos de js de novo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+// let n1 = document.querySelector(".numero1");
+// console.log(".numero1");
+// let n2 = document.querySelector(".numero2");
+// console.log(".numero2");
+// let resultado = n1 + n2;
+
+
+// console.log(resultado)
+
+n1 = window.prompt("Digite o primeiro número");
+n2 = window.prompt("Digite o segundo número");
+resultado = n1 + n2;
+
+window.alert(resultado);
