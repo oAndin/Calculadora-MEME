bd4a4cd260ccaf3ddad1cb5822c78a2ca22469fe
oAndin <00andherson@gmail.com>
2023-05-27 02:23:03 -0300

    Atualizando o JS

--- index.js
+++ index.js
@@ -5,15 +5,36 @@ console.log("Hello World!");
 // let resultado = n1 + n2;
 
 
-function soma(){ 
-    let n1 = document.getElementsByClassName("numero1");
-    let n2 = document.getElementsByClassName("numero2");
-    let resultado = n1 + n2;
-    document.getElementsByClassName("resultado")="resultado"
-    }
+// function soma(){ 
+//     let n1 = document.getElementById("numero1");
+//     let n2 = document.getElementById("numero2");
+//     let resultado = n1 + n2;
+//     document.getElementsByClassName("resultado")="resultado"
+//     }
 
-document.getElementsByClassName("myBtn").addEventListener("click", soma());
+// const myBtn = document.querySelector("button");
+const myBtn = document.querySelector("button")[0];
+console.log(myBtn); 
+ // get the value of an input
+ // get the input
+// let n1 = document.querySelector(".numero1");
+// console.log(n1);
 
+const getValue1 = document.querySelector(".numero1".value);
+console.log(getValue1);
+
+
+
+// let n2 = document.querySelector(".numero2");
+// console.log(n2);
+const getValue2 = document.querySelector(".numero2".value);
+console.log(getValue2);
+
+// myBtn.addEventListener("click", clicou);
+
+// function clicou() {
+//     console.log("Clicou");
+// }
 // n1 = window.prompt("Digite o primeiro número");
 // n2 = window.prompt("Digite o segundo número");
 // resultado = n1 + n2;
